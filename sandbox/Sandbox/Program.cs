88306643ba2032bfa6ee0fe4b0a0b3fbc0c0f2a3
Sadie Parker <sadieparker@byui.edu>
2024-03-18 21:42:47 -0600

    Changed back to sandbow

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,12 +5,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name");
-        string firstname = Console.ReadLine();
-        Console.Write("What is your last name?");
-        string lastname = Console.ReadLine();
-
-        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}");
-
+        Console.WriteLine("Hello Sandbox World!");
     }
 }
