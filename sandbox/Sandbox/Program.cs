240ba06aeef65024915fb157089d2374c430d82f
Sadie Parker <sadieparker@byui.edu>
2024-03-18 21:40:02 -0600

    James Bond Assignment Completed

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.Write("What is your first name");
+        string firstname = Console.ReadLine();
+        Console.Write("What is your last name?");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}");
+
     }
 }
