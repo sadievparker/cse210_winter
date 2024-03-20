654300e5e18bf305d9c0ed1adc5873f53e5ad055
Sadie Parker <sadieparker@byui.edu>
2024-03-19 18:22:23 -0600

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Guess the magic number!");
+        Random randonGen = new Random();
+        int magicNum = randonGen.Next(1,11);
+
+        int guess = -1;
+        
+
+        while ( guess != magicNum)
+        {
+            Console.Write("What is your guess? ");
+            string userInput = Console.ReadLine();
+            guess = int.Parse(userInput);
+
+            if (guess > magicNum )
+            {
+                
+                Console.WriteLine("Too high");
+            }
+            else if ( guess < magicNum )
+            {
+                Console.WriteLine("Too low");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }
     }
 }
