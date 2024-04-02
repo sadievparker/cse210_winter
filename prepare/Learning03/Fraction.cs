fce02d7dd8d29d175a1a78de12e173fd166c2217
Sadie Parker <sadieparker@byui.edu>
2024-04-01 22:19:17 -0600

    Finished learning 03

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,35 @@
+using System;
+
+public class Fraction 
+{
+    private int _topnum;
+    private int _bottom;
+    public Fraction()
+    {
+        _topnum = 1;
+        _bottom = -1;
+    }
+    public Fraction(int wholeNumber)
+    {
+        _topnum = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _topnum = top;
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_topnum}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_topnum / (double)_bottom;
+    }
+}
+
