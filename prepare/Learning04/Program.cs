2a395e0a495d6f7a1a0c0d2d278fd229ed77cec3
Sadie Parker <sadieparker@byui.edu>
2024-04-08 19:02:31 -0600

    finished learning 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,16 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Sadie Parker", "Linear Algebra");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Dallin Archibald", "Fractions", "1.8", "2-7");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Summer Miller", "English", "How to establish world peace.");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
